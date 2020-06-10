9b155cc04d07fcc9c19aeb915d0b1bb2a794a41a
Andrew Speed <aspeed@cloudsmith.io>
2020-06-10 17:23:03 +0100

    Add terraform API bindings for Go

--- bindings/go/src/configuration.go
+++ bindings/go/src/configuration.go
@@ -42,7 +42,7 @@ func NewConfiguration() *Configuration {
 		DefaultHeader: make(map[string]string),
 		APIKey:        make(map[string]string),
 		APIKeyPrefix:  make(map[string]string),
-		UserAgent:     "Swagger-Codegen/0.51.34/go",
+		UserAgent:     "Swagger-Codegen/0.51.35/go",
 		APIClient:     &APIClient{},
 	}
 
